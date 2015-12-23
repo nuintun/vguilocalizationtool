69e6d485cca7d04292ad9aedfb0002a78484d037
nuintun <nuintun@gmail.com>
2015-12-23 18:37:56 +0800

    update files

--- LocalizationData.cs
+++ LocalizationData.cs
@@ -7,22 +7,31 @@ namespace VGUILocalizationTool
 {
   class LocalizationData
   {
-    public string DelimeterID { get; set; }
+    public string BOF { get; set; }
 
     public string ID { get; set; }
-
-    public bool UseSlashN { get; set; }
-
-    public object DelimeterOrigin { get; set; }
-
-    public string Origin { get; set; }
-
-    public string OriginOld { get; set; }
-
-    public bool OriginTextChanged { get; set; }
 
     public string DelimeterLocalized { get; set; }
 
     public string Localized { get; set; }
+
+    public string DelimeterPlatform { get; set; }
+
+    public string Platform { get; set; }
+
+    public string EOF { get; set; }
+
+    public string Origin { get; set; }
+
+    public bool UseSlashN { get; set; }
+
+    // 下面无用
+    public bool OriginTextChanged { get; set; }
+
+    public string DelimeterID { get; set; }
+
+    public object DelimeterOrigin { get; set; }
+
+    public string OriginOld { get; set; }
   }
 }
--- ValveLocalizationFile.cs
+++ ValveLocalizationFile.cs
@@ -18,6 +18,9 @@ namespace VGUILocalizationTool
     public bool DontSaveNotLocalized { get; set; }
     private static Regex ORITOKENSRE = new Regex(@"^\[[\w_]+\]");
 
+    private string BOF = string.Empty;
+    private string EOF = string.Empty;
+
     // VGUI 解析和写入类
     public ValveLocalizationFile(string file)
     {
@@ -138,257 +141,419 @@ namespace VGUILocalizationTool
       return -1;
     }
 
+    private bool IsQuotationWrap(string token)
+    {
+      return token.StartsWith("\"") && token.EndsWith("\"");
+    }
+
     // 数据读取
     public List<LocalizationData> ReadData(string local = null)
     {
-      bool isOrigin = false;
+      bool isReadOrigin = false;
 
       if (local == null)
       {
-        local = tokens;
-        isOrigin = true;
+        local = this.tokens;
+        isReadOrigin = true;
       }
 
-      int l = 0;
-      string fileName = GetLocalFileName(local);
-      List<LocalizationData> data = new List<LocalizationData>();
+      string path = GetLocalFileName(local);
+      List<LocalizationData> list = new List<LocalizationData>();
 
       WithoutLang = false;
       WithOriginText = false;
 
-      if (File.Exists(fileName))
+      if (File.Exists(path))
       {
-        String line;
-        StreamReader sr = new StreamReader(fileName);
+        bool isInLanguage = false;
+        bool isPreEnterTokens = false;
+        bool isEnterTokens = false;
+        bool isLeaveTokens = false;
+        Tokens tokensParser = new Tokens();
+        string[][] tokens = tokensParser.GetTokens(path);
 
-        while ((line = sr.ReadLine()) != null)
+        for (int i = 0, rows = tokens.Length; i < rows; i++)
         {
-          bool ML = false;
-          bool unterm = false;
-          string[] tokens = SplitWithQuotas(line, ref unterm);
+          int pos = 0;
+          LocalizationData item = null;
 
-          if (unterm)
+          if (isEnterTokens)
           {
-            bool q = false;
-            int idx = tokens.Length - 1;
-
-            do
-            {
-              line = sr.ReadLine();
-
-              if (line == null)
-              {
-                break;
-              }
-
-              ML = true;
-
-              if (line.Length > 0)
-              {
-                for (int ci = line.Length - 1; ci >= 0; ci--)
-                {
-                  if (line[ci] == ' ' || line[ci] == '\t')
-                  {
-                    continue;
-                  }
-                  else if (line[ci] == '\"' && (ci == 0 || line[ci - 1] != '\\'))
-                  {
-                    q = true;
-                    tokens[idx] += Environment.NewLine + line.Substring(0, ci);
-                    break;
-                  }
-                  else
-                  {
-                    q = false;
-                    tokens[idx] += Environment.NewLine + line;
-                    break;
-                  }
-                }
-              }
-              else
-              {
-                tokens[idx] += Environment.NewLine + line;
-              }
-            } while (!q);
+            item = new LocalizationData();
           }
 
-          int i = SkipEmpty(tokens, 0);
-
-          switch (l)
+          for (int j = 0, cols = tokens[i].Length; j < cols; j++)
           {
-            case 0:
-              if (i == -1 || tokens[i].StartsWith("//"))
-              {
-                continue;
-              }
+            string token = tokens[i][j];
 
-              if (tokens[i] == "lang")
-              {
-                l++;
-                WithoutLang = false;
-              }
-              else if (tokens[i] == "Language")
-              {
-                l = 3;
-                WithoutLang = true;
-              }
-              break;
-            case 1:
-              if (i == -1 || tokens[i].StartsWith("//"))
-              {
-                continue;
-              }
+            if (isEnterTokens)
+            {
+              string trimToken = token.Trim();
 
-              if (tokens[i] == "{")
+              if (token == "}")
               {
-                l++;
-              }
-              break;
-            case 2:
-              if (i == -1 || tokens[i].StartsWith("//"))
-              {
-                continue;
-              }
+                isLeaveTokens = true;
 
-              if (tokens[i] == "Language")
-              {
-                l++;
-              }
-              break;
-            case 3:
-              if (i == -1 || tokens[i].StartsWith("//"))
-              {
-                continue;
-              }
-
-              if (tokens[i] == "Tokens")
-              {
-                l++;
-              }
-              break;
-            case 4:
-              if (i == -1 || tokens[i].StartsWith("//"))
-              {
-                continue;
-              }
-
-              if (tokens[i] == "{")
-              {
-                l++;
-              }
-              break;
-            case 5:
-              LocalizationData cd;
-
-              if (i == -1 || tokens[i].StartsWith("//"))
-              {
-                cd = new LocalizationData();
-
-                if (tokens.Length > 0)
+                if (j == cols - 1)
                 {
-                  cd.DelimeterID = line;
+                  this.EOF += Environment.NewLine;
                 }
-
-                data.Add(cd);
-                continue;
               }
-
-              if (tokens[i] == "}")
+              else if (trimToken != "")
               {
-                l++;
-                continue;
-              }
-
-              //根据不同情况解析 [origin] 类型的语言字段
-              int j = i + 2;
-              bool ori = false;
-              string s = tokens[i];
-
-              if (isOrigin)
-              {
-                // 不解析原始语言中的原始语言
-                if (ORITOKENSRE.IsMatch(s))
+                switch (pos)
                 {
-                  break;
+                  //字段
+                  case 0:
+                    if (IsQuotationWrap(token))
+                    {
+                      // 默认文件不读取带 [language] 字段
+                      if (isReadOrigin && ORITOKENSRE.IsMatch(token))
+                      {
+                        continue;
+                      }
+                      else
+                      {
+                        pos++;
+                        item.ID = token;
+                      }
+                    }
+                    break;
+                  // 语言
+                  case 1:
+                    if (IsQuotationWrap(token))
+                    {
+                      pos++;
+                      item.Origin = token;
+                    }
+                    break;
+                  // 平台
+                  case 2:
+                    if (token.StartsWith("[") && token.EndsWith("]"))
+                    {
+                      pos++;
+                    }
+                    break;
                 }
               }
               else
               {
-                // 解析本地化语言中的原始语言
-                string oriTokens = "[" + this.tokens + "]";
 
-                ori = s.StartsWith(oriTokens);
-
-                if (ori)
-                {
-                  s = s.Remove(0, oriTokens.Length);
-                }
               }
-
-              cd = (
-                from d in data
-                where d.ID == s
-                select d
-              ).SingleOrDefault();
-
-              if (cd == null)
+            }
+            else
+            {
+              if (token == "\"Tokens\"")
               {
-                cd = new LocalizationData();
-
-                if (i > 0)
-                {
-                  cd.DelimeterID = tokens[0];
-                }
-
-                cd.ID = s;
-                data.Add(cd);
+                isPreEnterTokens = true;
               }
-
-              if (j < tokens.Length)
+              else if (token == "{" && isPreEnterTokens)
               {
-                if (tokens[j].IndexOf("\\n") >= 0)
-                {
-                  cd.UseSlashN = true;
-                  tokens[j] = tokens[j].Replace("\\n", Environment.NewLine);
-                }
-                else
-                {
-                  cd.UseSlashN = (ML ? false : true);
-                }
+                isEnterTokens = true;
 
-                tokens[j] = tokens[j].Replace("\\\"", "\"");
-
-                if (ori)
+                if (j == cols - 1)
                 {
-                  WithOriginText = true;
-                  cd.Origin = tokens[j];
-                  cd.DelimeterOrigin = tokens[j - 1];
-                }
-                else
-                {
-                  cd.Localized = tokens[j];
-                  cd.DelimeterLocalized = tokens[j - 1];
+                  this.BOF += token + Environment.NewLine;
                 }
               }
-              break;
-            case 6:
-              if (i == -1 || tokens[i].StartsWith("//"))
+              else if (token == "\"Language\"")
               {
-                continue;
+                isInLanguage = true;
               }
-
-              if (tokens[i] == "{")
+              else if (isInLanguage && IsQuotationWrap(token))
               {
-                l++;
+                isInLanguage = false;
+                this.BOF += "\"{0}\"";
               }
-              break;
+              else
+              {
+                this.BOF += token;
+              }
+            }
+
+            if (isLeaveTokens)
+            {
+              this.EOF += token;
+            }
+          }
+
+          if (!isEnterTokens)
+          {
+            this.BOF += Environment.NewLine;
+          }
+          else
+          {
+            if (pos >= 2)
+            {
+              string tokenInID = "[" + this.tokens + "]";
+
+              if (item.ID.StartsWith(tokenInID))
+              {
+                WithOriginText = true;
+
+                LocalizationData selectedItem = (
+                  from it in list
+                  where it.ID == item.ID && it.Platform == item.Platform
+                  select it
+                ).SingleOrDefault();
+
+                if (selectedItem != null)
+                {
+                  selectedItem.Origin = item.Origin;
+                }
+              }
+              else
+              {
+                list.Add(item);
+              }
+            }
+          }
+
+          if (isLeaveTokens)
+          {
+            if (i < rows - 1)
+            {
+              this.EOF += Environment.NewLine;
+            }
           }
         }
-
-        sr.Close();
       }
 
-      return data;
+      return list;
+      // if (File.Exists(path))
+      // {
+      //  int l = 0;
+      //  String line;
+      //  StreamReader sr = new StreamReader(path);
+
+      //  while ((line = sr.ReadLine()) != null)
+      //  {
+      //    bool ML = false;
+      //    bool unterm = false;
+      //    string[] tokens = SplitWithQuotas(line, ref unterm);
+
+      //    if (unterm)
+      //    {
+      //      bool q = false;
+      //      int idx = tokens.Length - 1;
+
+      //      do
+      //      {
+      //        line = sr.ReadLine();
+
+      //        if (line == null)
+      //        {
+      //          break;
+      //        }
+
+      //        ML = true;
+
+      //        if (line.Length > 0)
+      //        {
+      //          for (int ci = line.Length - 1; ci >= 0; ci--)
+      //          {
+      //            if (line[ci] == ' ' || line[ci] == '\t')
+      //            {
+      //              continue;
+      //            }
+      //            else if (line[ci] == '\"' && (ci == 0 || line[ci - 1] != '\\'))
+      //            {
+      //              q = true;
+      //              tokens[idx] += Environment.NewLine + line.Substring(0, ci);
+      //              break;
+      //            }
+      //            else
+      //            {
+      //              q = false;
+      //              tokens[idx] += Environment.NewLine + line;
+      //              break;
+      //            }
+      //          }
+      //        }
+      //        else
+      //        {
+      //          tokens[idx] += Environment.NewLine + line;
+      //        }
+      //      } while (!q);
+      //    }
+
+      //    int i = SkipEmpty(tokens, 0);
+
+      //    switch (l)
+      //    {
+      //      case 0:
+      //        if (i == -1 || tokens[i].StartsWith("//"))
+      //        {
+      //          continue;
+      //        }
+
+      //        if (tokens[i] == "lang")
+      //        {
+      //          l++;
+      //          WithoutLang = false;
+      //        }
+      //        else if (tokens[i] == "Language")
+      //        {
+      //          l = 3;
+      //          WithoutLang = true;
+      //        }
+      //        break;
+      //      case 1:
+      //        if (i == -1 || tokens[i].StartsWith("//"))
+      //        {
+      //          continue;
+      //        }
+
+      //        if (tokens[i] == "{")
+      //        {
+      //          l++;
+      //        }
+      //        break;
+      //      case 2:
+      //        if (i == -1 || tokens[i].StartsWith("//"))
+      //        {
+      //          continue;
+      //        }
+
+      //        if (tokens[i] == "Language")
+      //        {
+      //          l++;
+      //        }
+      //        break;
+      //      case 3:
+      //        if (i == -1 || tokens[i].StartsWith("//"))
+      //        {
+      //          continue;
+      //        }
+
+      //        if (tokens[i] == "Tokens")
+      //        {
+      //          l++;
+      //        }
+      //        break;
+      //      case 4:
+      //        if (i == -1 || tokens[i].StartsWith("//"))
+      //        {
+      //          continue;
+      //        }
+
+      //        if (tokens[i] == "{")
+      //        {
+      //          l++;
+      //        }
+      //        break;
+      //      case 5:
+      //        LocalizationData cd;
+
+      //        if (i == -1 || tokens[i].StartsWith("//"))
+      //        {
+      //          cd = new LocalizationData();
+
+      //          if (tokens.Length > 0)
+      //          {
+      //            cd.DelimeterID = line;
+      //          }
+
+      //          data.Add(cd);
+      //          continue;
+      //        }
+
+      //        if (tokens[i] == "}")
+      //        {
+      //          l++;
+      //          continue;
+      //        }
+
+      //        //根据不同情况解析 [origin] 类型的语言字段
+      //        int j = i + 2;
+      //        bool ori = false;
+      //        string s = tokens[i];
+
+      //        if (isOrigin)
+      //        {
+      //          // 不解析原始语言中的原始语言
+      //          if (ORITOKENSRE.IsMatch(s))
+      //          {
+      //            break;
+      //          }
+      //        }
+      //        else
+      //        {
+      //          // 解析本地化语言中的原始语言
+      //          string oriTokens = "[" + this.tokens + "]";
+
+      //          ori = s.StartsWith(oriTokens);
+
+      //          if (ori)
+      //          {
+      //            s = s.Remove(0, oriTokens.Length);
+      //          }
+      //        }
+
+      //        cd = (
+      //          from d in data
+      //          where d.ID == s
+      //          select d
+      //        ).SingleOrDefault();
+
+      //        if (cd == null)
+      //        {
+      //          cd = new LocalizationData();
+
+      //          if (i > 0)
+      //          {
+      //            cd.DelimeterID = tokens[0];
+      //          }
+
+      //          cd.ID = s;
+      //          data.Add(cd);
+      //        }
+
+      //        if (j < tokens.Length)
+      //        {
+      //          if (tokens[j].IndexOf("\\n") >= 0)
+      //          {
+      //            cd.UseSlashN = true;
+      //            tokens[j] = tokens[j].Replace("\\n", Environment.NewLine);
+      //          }
+      //          else
+      //          {
+      //            cd.UseSlashN = (ML ? false : true);
+      //          }
+
+      //          tokens[j] = tokens[j].Replace("\\\"", "\"");
+
+      //          if (ori)
+      //          {
+      //            WithOriginText = true;
+      //            cd.Origin = tokens[j];
+      //            cd.DelimeterOrigin = tokens[j - 1];
+      //          }
+      //          else
+      //          {
+      //            cd.Localized = tokens[j];
+      //            cd.DelimeterLocalized = tokens[j - 1];
+      //          }
+      //        }
+      //        break;
+      //      case 6:
+      //        if (i == -1 || tokens[i].StartsWith("//"))
+      //        {
+      //          continue;
+      //        }
+
+      //        if (tokens[i] == "{")
+      //        {
+      //          l++;
+      //        }
+      //        break;
+      //    }
+      //  }
+
+      //  sr.Close();
+      //}
+
+      //return data;
     }
 
     // 数据写入
